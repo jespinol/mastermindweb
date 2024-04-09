8979d21a3aae974e39f866a547a74a0fac3fc5bd
jespinol <76534045+jespinol@users.noreply.github.com>
2024-04-08 21:58:01 -0400

    Initial commit

---
+++ src/test/java/org/jmel/mastermindweb/MastermindWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jmel.mastermindweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MastermindWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
